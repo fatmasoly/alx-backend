a07ba6ad91b50acde067993192da09a7b4d47641
fatma soliman <fatmacivil@hotmail.com>
2024-07-24 12:48:50 +0300

    first commit

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+""" BasicCache module"""
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """ BasicCache defines:"""
+    
+    def __init__(self):
+        """ Constructor """
+        super().__init__()
+        
+    def put(self, key, item):
+        """ Put method """
+        if item is None or key is None:
+            return
+        
+        self.cache_data[key] = item
+        
+    def get(self, key):
+        """ Get method """
+        if key is None or not self.cache_data.get(key):
+            return None
+        
+        return self.cache_data.get(key)
