e820587ad188d447f043722da400f9d04467b757
fatma soliman <fatmacivil@hotmail.com>
2024-07-30 20:16:37 +0300

    fix

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -6,20 +6,21 @@ from flask_babel import Babel
 
 class Config:
     """Config class"""
-    LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app = Flask(__name__)
 babel = Babel(app)
-app.config.from_object('config')
+app.config.from_object('Config')
 
 
 @app.route('/', strict_slashes=False)
 def index() -> str:
     """index page"""
     return render_template('1-index.html')
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
